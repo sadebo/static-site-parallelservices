02c798dceade67a884f92af49625273a8e78ea60
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-11-02 12:39:02 -0500

    deploy static website

--- modules/cloudflare_dns/providers.tf
+++ modules/cloudflare_dns/providers.tf
@@ -1,3 +1,12 @@
-# provider "cloudflare" {
-#   api_token = var.cloudflare_api_token
-# }
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}

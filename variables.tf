f25803c7053141acbd3f3d8623beea874e609c5e
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-11-02 13:01:42 -0500

    deploy static website

--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 variable "cloudflare_api_token" {
   description = "API token for managing DNS in Cloudflare"
+  type        = string
   sensitive   = true
+  default     = ""   # 👈 allows terraform plan/apply without token in Phase 1
 }
